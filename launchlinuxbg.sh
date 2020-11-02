453f65f2c0c37bdb7c319f45bd4d0060ef91f5f5
zemmsoares <zemmsoares@gmail.com>
2020-11-02 16:25:00 +0000

    initial commit

---
+++ launchlinuxbg.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd $HOME/.config/linuxbg/
+python linuxbg.py
---
+++ linuxbg.py
@@ -0,0 +1,28 @@
+#import required
+
+import platform, distro
+from PIL import Image, ImageDraw, ImageFont
+
+kernelpre = "カーネル :"
+kernelversion = platform.release().upper()
+node = distro.id().upper()
+system = platform.system().upper()
+
+image = Image.open('background.png')
+draw = ImageDraw.Draw(image)
+
+font = ImageFont.truetype('HanaMinA.ttf', size=20)
+font2 = ImageFont.truetype('DejaVuSerifCondensed.ttf', size=55)
+
+kernel = kernelpre + kernelversion
+distro = node +' '+ system
+
+W,H = 1920, 1080
+w,h = font.getsize(kernel)
+draw.text(((W-w)/2,(H-h)/2), kernel, font=font, fill="black")
+
+X,Y = 1920, 1080
+x,y = font2.getsize(distro)
+draw.text(((X-x)/2,(Y-y)/2-50), distro, font=font2, fill=(168,166,174))
+ 
+image.save('bg.png')
